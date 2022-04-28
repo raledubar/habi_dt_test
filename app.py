e0be02c24925a976847af44dbd7ad0d3647a7fd2
raledubar <raledubar@gmail.com>
2022-04-27 19:20:39 -0500

    Creating first query to show all data to user

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
 from flask import Flask, request, jsonify, make_response
-import pymysql
-import json
-
+import handlers
 
 app = Flask(__name__)
 
 
-def get_connection():
-    return pymysql.connect(
-        host='3.130.126.210',
-        port=3309,
-        user='pruebas',
-        password='VGbt3Day5R',
-        database='habi_db',
-    )
-
-
 @app.route('/inmuebles', methods=['GET'])
-def get_inmuebles():
-    connection = get_connection()
-    with connection.cursor() as cursor:
-        read = cursor.execute("SELECT * FROM habi_db.status_history")
-        if read:
-            print("read in terminal")
-            print(read)
-            return f"I have read the table"
-        return f"I CAN NOT READ THE TABLE"
+def get_inmuebles_api():
+    # inmuebles = handlers.get_inmuebles()
+    inmuebles = handlers.filter_inmuebles()
+    return make_response(jsonify({"inmuebles": inmuebles}))
 
 
 if __name__ == '__main__':
