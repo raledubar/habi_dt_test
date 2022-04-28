f61882b8765464aed60fb3517688200082c5b227
raledubar <raledubar@gmail.com>
2022-04-28 09:47:00 -0500

    Importing models and handlers. Keep creating filter with year, city and state

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, request, jsonify, make_response
 import handlers
+from models import Inmueble
+
 
 app = Flask(__name__)
 
 
 @app.route('/inmuebles', methods=['GET'])
 def get_inmuebles_api():
-    # inmuebles = handlers.get_inmuebles()
-    inmuebles = handlers.filter_inmuebles()
+    args = request.args
+    data = handlers.filter_inmuebles()
+    inmuebles = []
+    for row in data:
+        resource = Inmueble(*row)
+        entity = dict(resource)
+        inmuebles.append(entity)
+    if args:
+        resources = []
+        for resource in inmuebles:
+            if handlers.check_filters(
+                resource=resource,
+                filters=args
+            ):
+                resources.append(resource)
+        return make_response(jsonify({"inmuebles": resources}))
     return make_response(jsonify({"inmuebles": inmuebles}))
 
 
